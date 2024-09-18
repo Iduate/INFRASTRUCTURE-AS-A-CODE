803fc202759f3472ca888b5b382edcb84ee76cfb
XerdocsTechTeam <96699888+XerdocsTechTeam@users.noreply.github.com>
2024-09-18 10:56:41 +0100

    IAAC

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.3"
+    }
+  }
+}
+
+
+
+
+
+
+
+
